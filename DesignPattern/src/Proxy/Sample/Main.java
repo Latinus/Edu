41ff0facd5535695a0441e21881fd4ade1917c30
Latinus <latinus815@gmail.com>
2016-10-21 16:56:27 +0900

    add Patterns

---
+++ DesignPattern/src/Proxy/Sample/Main.java
@@ -0,0 +1,11 @@
+package Proxy.Sample;
+
+public class Main {
+    public static void main(String[] args) {
+        Printable p = new PrinterProxy("Alice");
+        System.out.println("현재의 이름은" + p.getPrinterName() + "입니다.");
+        p.setPrinterName("Bob");
+        System.out.println("현재의 이름은" + p.getPrinterName() + "입니다.");
+        p.print("Hello, world.");
+    }
+}
